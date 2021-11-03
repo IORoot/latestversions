c5377c10eb067acb33810c79da262f57b7953d46
Andy Pearson <andy@londonparkour.com>
2021-11-03 17:38:31 +0000

    linux

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-node --no-warnings ./osmosis/microsoft_windows.js
+node --no-warnings ./x-ray/apple_macos.js
+node --no-warnings ./x-ray/microsoft_windows.js
+node --no-warnings ./x-ray/linux_ubuntu.js
+node --no-warnings ./x-ray/linux_pop_os.js
+node --no-warnings ./x-ray/linux_centos.js
+node --no-warnings ./x-ray/linux_arch.js
---
+++ x-ray/linux_arch.js
@@ -0,0 +1,34 @@
+const fs   = require('fs');
+const Xray = require('x-ray')
+const xray = Xray()
+
+var url = 'https://distrowatch.com/table.php?distribution=arch';
+var selector = 'body > table:nth-child(8) > tr:nth-child(1) > td:nth-child(3)'
+var download = 'https://archlinux.org/';
+var folders = './results/os/linux'
+var filename = 'arch.json'
+
+xray(url, selector)(function(err, returned) {
+
+    // console.log(err);
+    // console.log(returned);
+
+  var json = {
+    "latest_version": returned,
+    "html_url": download,
+  };
+
+  const data = JSON.stringify(json);
+
+  if (!fs.existsSync(folders)){
+    fs.mkdirSync(folders, { recursive: true });
+  }
+
+  fs.writeFile(folders+'/'+filename, data, (err) => {
+    if (err) {
+        throw err;
+    }
+    console.log(folders+'/'+filename);
+});
+
+});
