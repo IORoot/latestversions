f741927271df5fb7be78d9535c6e74e3aebdc9ae
Andy Pearson <andy@londonparkour.com>
2022-05-16 15:03:21 +0100

    DB

--- x-ray/microsoft_windows.js
+++ x-ray/microsoft_windows.js
@@ -1,6 +1,7 @@
-const fs   = require('fs');
-const Xray = require('x-ray')
-const xray = Xray()
+const fs     = require('fs');
+const Xray   = require('x-ray')
+const xray   = Xray()
+const Client = require('pg');
 
 var url      = 'https://en.wikipedia.org/wiki/Microsoft_Windows';
 var selector = '#mw-content-text > div.mw-parser-output > table.infobox.vevent > tbody > tr:nth-child(5) > td'
@@ -35,4 +36,22 @@ xray(url, selector)(function(err, returned) {
     
   });
 
+});
+
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
 });
