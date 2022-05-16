f72b6956eb63055d3fc6a3fe78a82180a5a51992
Andy Pearson <andy@londonparkour.com>
2022-05-16 14:01:51 +0100

    Regex

--- x-ray/microsoft_windows.js
+++ x-ray/microsoft_windows.js
@@ -2,10 +2,11 @@ const fs   = require('fs');
 const Xray = require('x-ray')
 const xray = Xray()
 
-var url = 'https://en.wikipedia.org/wiki/Microsoft_Windows';
+var url      = 'https://en.wikipedia.org/wiki/Microsoft_Windows';
 var selector = '#mw-content-text > div.mw-parser-output > table.infobox.vevent > tbody > tr:nth-child(5) > td'
+const regex  = /\s.*/; // Match a whitespace and remove everything else.
 var download = 'https://www.microsoft.com/en-gb/software-download';
-var folders = './results/os/microsoft'
+var folders  = './results/os/microsoft'
 var filename = 'windows.json'
 
 
@@ -16,7 +17,9 @@ xray(url, selector)(function(err, returned) {
     "html_url": download,
   };
 
-  const data = JSON.stringify(json);
+  var data = JSON.stringify(json);
+
+  var data = data.replace(regex,'');
 
   if (!fs.existsSync(folders)){
     fs.mkdirSync(folders, { recursive: true });
