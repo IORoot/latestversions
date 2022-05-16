29efb15ca9eb443a47d9db792a827d09179354a4
Andy Pearson <andy@londonparkour.com>
2022-05-16 18:12:55 +0100

    date

--- x-ray/microsoft_windows.js
+++ x-ray/microsoft_windows.js
@@ -1,26 +1,30 @@
 require('dotenv').config();
-
 const Xray   = require('x-ray')
 const xray   = Xray()
+const base = require('airtable').base('app8NMPBTR6QCoYX2');
 
+
+var id       = 'os-microsoft-windows';
 var title    = 'Microsoft Windows';
 var url      = 'https://en.wikipedia.org/wiki/Microsoft_Windows';
 var selector = '#mw-content-text > div.mw-parser-output > table.infobox.vevent > tbody > tr:nth-child(5) > td'
 var regex    = /\s.*/; // Match a whitespace and remove everything else.
 var download = 'https://www.microsoft.com/en-gb/software-download';
-var folders  = './results/os/microsoft'
-var filename = 'windows.json'
 
-const base = require('airtable').base('app8NMPBTR6QCoYX2');
 
+// Get Result and push to Airtable.
 xray(url, selector)(function(err, returned) {
 
   var version = returned.replace(regex,'');
 
+  const date = new Date()
+
   base('versions').create({
-    "title": title,
+    "id"     : id,
+    "title"  : title,
     "version": version,
-    "link": download
+    "link"   : download,
+    "date"   : date.toGMTString()
   }, function(err, record) {
     if (err) {
       console.error(err);
@@ -29,4 +33,3 @@ xray(url, selector)(function(err, returned) {
   });
 
 });
-
