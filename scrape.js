e28e5449eb3acd264cfa766ef24bc7d109eb4550
Andy Pearson <andy@londonparkour.com>
2022-05-30 10:50:14 +0100

    more info

--- scrape.js
+++ scrape.js
@@ -2,28 +2,37 @@ require('dotenv').config();
 const fs = require('fs');
 const Xray   = require('x-ray')
 const xray   = Xray()
-const base = require('airtable').base('app8NMPBTR6QCoYX2');
 
 let rawdata = fs.readFileSync('targets.json');
 const targets = JSON.parse(rawdata);
 
-targets.forEach(target => {
+const versions = []
 
+targets.forEach(target => {
+  
   xray(target.url, target.selector)(function(err, returned) {
 
     var regex = new RegExp(target.regex);
     var version = returned.replace(regex,'');
     const date = new Date()
 
-    base('versions').create({
-      "id"     : target.id,
-      "title"  : target.title,
-      "version": version,
-      "link"   : target.download,
-      "date"   : date.toGMTString()
-    }, function(err, record) {
-      if (err) {  console.error(err); return; }
+    versions.push({
+      category: target.category,
+      company:  target.company,
+      title:    target.title,
+      version:  version,
+      link:     target.download,
+      date:     date.toGMTString(),
+      logo_url: target.logo_url,
+      colour1:  target.colour1,
+      colour2:  target.colour2,
     });
+    var json = JSON.stringify(versions);
+
+    fs.writeFile('results.json', json, 'utf8', function(err) {
+      if (err) throw err;
+    }
+  );
 
   })
 
